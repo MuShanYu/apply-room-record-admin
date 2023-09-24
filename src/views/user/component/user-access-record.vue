9f79326ffd331b13299ac4a9d125953e2936770d
StriveCar <108204236+StriveCar@users.noreply.github.com>
2023-09-24 18:24:41 +0800

    统计结果增加近一周打卡时间

--- src/views/user/component/user-access-record.vue
+++ src/views/user/component/user-access-record.vue
@@ -6,13 +6,7 @@
       </el-button>
     </div>
     <div v-show="!seeCount">
-      <el-table
-        key="3"
-        v-loading="listLoading"
-        :data="recordList"
-        border
-        fit
-        highlight-current-row
+      <el-table key="3" v-loading="listLoading" :data="recordList" border fit highlight-current-row
         style="width: 100%;">
         <el-table-column label="校区" width="150" align="center">
           <template slot-scope="{row}">
@@ -59,24 +53,18 @@
         </el-table-column>
         <el-table-column label="操作" align="center">
           <template slot-scope="{row, $index}">
-            <el-button plain type="danger" @click="handleDelRecord(row, $index)" v-waves
-                       v-permission="['super-admin']" style="margin: 3px;" size="mini">
+            <el-button plain type="danger" @click="handleDelRecord(row, $index)" v-waves v-permission="['super-admin']"
+              style="margin: 3px;" size="mini">
               {{row.state === 1 ? '删除记录' : '取消删除'}}
             </el-button>
           </template>
         </el-table-column>
       </el-table>
       <pagination v-show="total>0" :total="total" :page.sync="query.page" :limit.sync="query.size"
-                  @pagination="getUserAccessRecordListAdmin"/>
+        @pagination="getUserAccessRecordListAdmin" />
     </div>
     <div v-show="seeCount">
-      <el-table
-        key="3"
-        v-loading="countLoading"
-        :data="countList"
-        border
-        fit
-        highlight-current-row
+      <el-table key="3" v-loading="countLoading" :data="countList" border fit highlight-current-row
         style="width: 100%;">
         <el-table-column label="校区" width="150" align="center">
           <template slot-scope="{row}">
@@ -98,6 +86,11 @@
             <span>{{ row.category }}</span>
           </template>
         </el-table-column>
+        <el-table-column label="签到时长(近一周)" align="center">
+          <template slot-scope="{row}">
+            <span>{{ allTime }}</span>
+          </template>
+        </el-table-column>
         <el-table-column label="扫码进入次数" align="center">
           <template slot-scope="{row}">
             <span>{{ row.entryTimes }}</span>
@@ -110,112 +103,133 @@
         </el-table-column>
       </el-table>
       <pagination v-show="countTotal>0" :total="countTotal" :page.sync="query.page" :limit.sync="query.size"
-                  @pagination="getUserAccessCountAdmin"/>
+        @pagination="getUserAccessCountAdmin" />
     </div>
   </div>
 </template>
 
 <script>
-import Pagination from "@/components/Pagination";
+  import Pagination from "@/components/Pagination";
 
-import recordApi from '@/api/record'
+  import recordApi from '@/api/record'
 
-export default {
-  name: "user-access-record",
-  props: {
-    id: {
-      type: String,
-      default: ''
-    }
-  },
-  components: {
-    Pagination
-  },
-  watch: {
-    id(val, oldVal) {
-      this.query.userId = val
-      this.getUserAccessRecordListAdmin()
-      this.seeCount = false
-    }
-  },
-  data() {
-    return {
-      query: {
-        page: 1,
-        size: 10,
-        userId: ''
-      },
-      countList: [],
-      recordList: [],
-      total: 0,
-      listLoading: false,
-      seeCount: false,
-      countTotal: 0,
-      countLoading: false,
-      btnLoading: false
-    }
-  },
-  created() {
-    this.query.userId = this.id
-    this.getUserAccessRecordListAdmin()
-  },
-  methods: {
-    getUserAccessCountAdmin() {
-      this.countLoading = true
-      this.btnLoading = true
-      recordApi.userAccessCountAdminApi(this.query).then(res => {
-        this.countList = res.pageData
-        this.countTotal = res.totalSize
-        this.countLoading = false
-        this.btnLoading = false
-        // console.log(this.countList);
-      }).catch(e => {
-        this.countLoading = false
-        this.btnLoading = false
-      })
+  export default {
+    name: "user-access-record",
+    props: {
+      id: {
+        type: String,
+        default: ''
+      }
     },
-    getUserAccessRecordListAdmin() {
-      this.listLoading = true
-      recordApi.userAccessRecordListAdminApi(this.query).then(res => {
-        this.recordList = res.pageData
-        this.total = res.totalSize
-        this.listLoading = false
-        // console.log(this.recordList);
-      }).catch(e => {
-        this.listLoading = false
-      })
+    components: {
+      Pagination
     },
-    handleDelRecord(row, index) {
-      let text = row.state === 1 ? '确定要删除该条记录吗?' : '确定要取消删除吗?'
-      this.$confirm(text, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        recordApi.delRecord(row.id).then(() => {
-          if (row.state === 1) {
-            row.state = -1
-            this.$message.success('删除成功')
-          } else {
-            row.state = 1
-            this.$message.success('取消删除成功')
-          }
-        })
-      }).catch(() => {
-
-      })
-    },
-    handleSeeCount() {
-      this.query.page = 1
-      if (this.seeCount) {
+    watch: {
+      id(val, oldVal) {
+        this.query.userId = val
+        this.getUserAccessRecordListAdmin()
         this.seeCount = false
-      } else {
-        this.seeCount = true
-        this.getUserAccessCountAdmin()
+      }
+    },
+    data() {
+      return {
+        query: {
+          page: 1,
+          size: 10,
+          userId: ''
+        },
+        countList: [],
+        recordList: [],
+        total: 0,
+        listLoading: false,
+        seeCount: false,
+        countTotal: 0,
+        countLoading: false,
+        btnLoading: false,
+        allTime: 0
+      }
+    },
+    created() {
+      this.query.userId = this.id
+      this.getUserAccessRecordListAdmin()
+    },
+    methods: {
+      getUserAccessCountAdmin() {
+        this.countLoading = true
+        this.btnLoading = true
+        recordApi.userAccessCountAdminApi(this.query).then(res => {
+          this.countList = res.pageData
+          this.countTotal = res.totalSize
+          this.countLoading = false
+          this.btnLoading = false
+          // console.log(this.countList);
+        }).catch(e => {
+          this.countLoading = false
+          this.btnLoading = false
+        })
+      },
+      getUserAccessRecordListAdmin() {
+        this.listLoading = true
+        recordApi.userAccessRecordListAdminApi(this.query).then(res => {
+          this.recordList = res.pageData
+          this.total = res.totalSize
+          this.listLoading = false
+          res.pageData.forEach(element => {
+            const currentTimeMillis = Date.now();
+            const oneWeekMillis = 7 * 24 * 60 * 60 * 1000;
+            const oneWeekAgoMillis = currentTimeMillis - oneWeekMillis;
+            const twoHour = 1440000;
+            if (element.entryTime >= currentTimeMillis) {
+              if (element.outTime != null) {
+                this.allTime += (element.outTime - element.entryTime)
+              } else if (currentTimeMillis - element.entryTime < twoHour) {
+                this.allTime += (currentTimeMillis - currentTimeMillis)
+              } else {
+                this.allTime += 1440000
+              }
+            } else if (element.outTime != null & element.outTime > oneWeekAgoMillis) {
+              this.allTime += (element.outTime - oneWeekAgoMillis)
+            } else if (element.outTime == null & oneWeekAgoMillis - element.entryTime < twoHour) {
+              this.allTime += (element.entryTime + twoHour - oneWeekAgoMillis)
+            }
+          });
+          this.allTime = (this.allTime / 1000 / 3600).toFixed(1)
+          // console.log(this.recordList);
+        }).catch(e => {
+          this.listLoading = false
+        })
+      },
+      handleDelRecord(row, index) {
+        let text = row.state === 1 ? '确定要删除该条记录吗?' : '确定要取消删除吗?'
+        this.$confirm(text, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          recordApi.delRecord(row.id).then(() => {
+            if (row.state === 1) {
+              row.state = -1
+              this.$message.success('删除成功')
+            } else {
+              row.state = 1
+              this.$message.success('取消删除成功')
+            }
+          })
+        }).catch(() => {
+
+        })
+      },
+      handleSeeCount() {
+        this.query.page = 1
+        if (this.seeCount) {
+          this.seeCount = false
+        } else {
+          this.seeCount = true
+          this.getUserAccessCountAdmin()
+        }
       }
     }
   }
-}
 </script>
 
 <style scoped>
