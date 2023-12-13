bb24e7f7fc45e7e2dd63bec3cf6885692ed88b16
yulf <yulf@startimes.com.cn>
2023-12-13 16:41:46 +0800

    生产环境暂时修改为测试服务器，用于测试使用，上线后修改回原值

--- src/common/sys-config.js
+++ src/common/sys-config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     userExcelHref: 'https://mushanyu-file-save.oss-cn-beijing.aliyuncs.com/project/apply-room-record/%E4%BA%BA%E5%91%98%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx'
   },
   production: {
-    baseUrl: 'https://www.mushanyu.xyz:8500',
+    baseUrl: 'http://101.42.135.147:8500',
     logo: 'https://www.mushanyu.xyz/logo.png',
     avatar: 'https://www.mushanyu.xyz/header1.gif',
     emptyImg: 'https://www.mushanyu.xyz/empty.png',
