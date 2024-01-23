5ed841220a1b7b9dd80a3420064a3938567b7ac6
yulf <yulf@startimes.com.cn>
2024-01-23 16:19:31 +0800

    修改模板下载路径

--- src/common/sys-config.js
+++ src/common/sys-config.js
@@ -6,8 +6,8 @@ const CONFIG = {
     avatar: 'http://localhost:9528/header1.gif',
     emptyImg: 'http://localhost:9528/empty.png',
     qrCodeUrlPrefix: '',
-    roomExcelHref: 'https://mushanyu-file-save.oss-cn-beijing.aliyuncs.com/project/apply-room-record/%E6%88%BF%E9%97%B4%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
-    userExcelHref: 'https://mushanyu-file-save.oss-cn-beijing.aliyuncs.com/project/apply-room-record/%E4%BA%BA%E5%91%98%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx'
+    roomExcelHref: 'https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/%E6%88%BF%E9%97%B4%E6%89%B9%E9%87%8F%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+    userExcelHref: 'https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/%E4%BA%BA%E5%91%98%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF-%E5%9D%87%E4%B8%BA%E5%BF%85%E5%A1%AB%E9%A1%B9.xlsx'
   },
   // 内网访问后端服务
   production: {
@@ -16,8 +16,8 @@ const CONFIG = {
     avatar: 'https://ip/header1.gif',
     emptyImg: 'https://ip/empty.png',
     qrCodeUrlPrefix: '',
-    roomExcelHref: 'https://mushanyu-file-save.oss-cn-beijing.aliyuncs.com/project/apply-room-record/%E6%88%BF%E9%97%B4%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
-    userExcelHref: 'https://mushanyu-file-save.oss-cn-beijing.aliyuncs.com/project/apply-room-record/%E4%BA%BA%E5%91%98%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx'
+    roomExcelHref: 'https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/%E6%88%BF%E9%97%B4%E6%89%B9%E9%87%8F%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF.xlsx',
+    userExcelHref: 'https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/%E4%BA%BA%E5%91%98%E5%AF%BC%E5%85%A5%E6%A8%A1%E6%9D%BF-%E5%9D%87%E4%B8%BA%E5%BF%85%E5%A1%AB%E9%A1%B9.xlsx'
   }
 }
 
