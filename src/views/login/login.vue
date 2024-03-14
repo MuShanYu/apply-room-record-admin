5cacf52cc4fc225f04874a7848a860aeb115e8d9
yulf <yulf@startimes.com.cn>
2024-03-14 11:48:06 +0800

    重构登录页面

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,104 +1,54 @@
 <template>
-  <div class="login-container">
-    <el-form
-        ref="loginForm"
-        :model="loginForm"
-        :rules="loginRules"
-        class="login-form"
-        auto-complete="on"
-        label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">登录</h3>
+  <div class="form-wrapper">
+    <div class="info-side">
+    </div>
+
+    <div class="form-side">
+      <div class="my-form">
+        <div class="form-welcome-row">
+          <div>
+            <img src="https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/logo.png" alt="Logo" class="logo">
+          </div>
+          <h1>房间预约与人员流动统计</h1>
+        </div>
+        <div class="divider">
+          <div class="divider-line"></div>
+          Manage
+          <div class="divider-line"></div>
+        </div>
+        <div class="text-field">
+          <label for="email">学号/工号</label>
+          <input maxlength="16" v-model="loginForm.stuNum" type="text" name="stuNum" autocomplete="off"
+                 placeholder="请输入您的学号/工号">
+        </div>
+        <div class="text-field">
+          <label for="password">密码</label>
+          <input maxlength="16" v-model="loginForm.pwd" type="password" name="password"
+                 placeholder="请输入您的登录密码">
+        </div>
+        <button @click="handleLogin" v-loading="loading" class="my-form__button" type="button">
+          登 录
+        </button>
+        <div class="rights"> ©2023 MuShanYu All Rights Reserved.</div>
       </div>
-
-      <el-form-item prop="stuNum">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-            ref="stuNum"
-            v-model="loginForm.stuNum"
-            placeholder="请输入学号/工号"
-            name="stuNum"
-            type="text"
-            tabindex="1"
-            auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="pwd">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-            :key="passwordType"
-            ref="pwd"
-            v-model="loginForm.pwd"
-            :type="passwordType"
-            placeholder="请输入登录密码"
-            name="pwd"
-            tabindex="2"
-            auto-complete="on"
-            @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon
-              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
-          />
-        </span>
-      </el-form-item>
-
-      <el-button
-          :loading="loading"
-          type="primary"
-          style="width: 100%; margin-bottom: 30px"
-          @click.native.prevent="handleLogin"
-      >登录</el-button
-      >
-
-      <div class="tips">
-        <span>密码和基础信息修改请在H5客户端进行！</span>
-      </div>
-
-      <div style="padding: 25px;display: flex;align-items: center;justify-content: center;margin-top: 150px;">
-        <a style="color: #909399;" href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2022007174号-3</a>
-      </div>
-
-    </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+import '@/styles/login.css'
+import fa from "element-ui/src/locale/lang/fa";
+
 export default {
   name: "Login",
   data() {
-    const validateTel = (rule, value, callback) => {
-      if (!/^[0-9]{6,16}$/.test(value.trim())) {
-        callback(new Error("请输入正确的学号"));
-      } else {
-        callback();
-      }
-    };
-    const validatePwd = (rule, value, callback) => {
-      if (value.length < 4 || value.length > 16) {
-        callback(new Error("密码的长度在4~16之间"));
-      } else {
-        callback();
-      }
-    };
     return {
       loginForm: {
         stuNum: "",
         pwd: "",
         device: 'pc'
       },
-      loginRules: {
-        stuNum: [{ required: true, trigger: "blur", validator: validateTel }],
-        pwd: [{ required: true, trigger: "blur", validator: validatePwd }],
-      },
       loading: false,
-      passwordType: "password",
       redirect: undefined,
     };
   },
@@ -111,145 +61,35 @@ export default {
     },
   },
   methods: {
-    showPwd() {
-      if (this.passwordType === "password") {
-        this.passwordType = "";
-      } else {
-        this.passwordType = "password";
+    checkForm() {
+      if (!/^[0-9]{6,16}$/.test(this.loginForm.stuNum.trim())) {
+        this.$message.error('请输入正确的学号')
+        return false
       }
-      this.$nextTick(() => {
-        this.$refs.pwd.focus();
-      });
+      if (this.loginForm.pwd.length < 4 || this.loginForm.pwd.length > 16) {
+        this.$message.error('密码的长度在4~16之间')
+        return false
+      }
+      return true
     },
     handleLogin() {
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.loading = true;
-          this.$store
-              .dispatch("user/login", this.loginForm)
-              .then(() => {
-                this.$router.push({ path: this.redirect || "/" });
-                this.loading = false;
-              })
-              .catch(() => {
-                this.loading = false;
-              });
-        } else {
-          return false;
-        }
-      });
+      if (this.checkForm()) {
+        this.loading = true;
+        this.$store.dispatch("user/login", this.loginForm)
+          .then(() => {
+            this.$router.push({path: this.redirect || "/"});
+            this.loading = false;
+          })
+          .catch(() => {
+            this.loading = false;
+          });
+      }
     },
   },
 };
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
+<style scoped>
 
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
 </style>
