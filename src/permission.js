8f3a0a1d6825bcca2de14321963e4959eb6056eb
yulf <yulf@startimes.com.cn>
2024-04-30 16:49:36 +0800

    优化未登录校验逻辑，避免，未带token请求

--- src/permission.js
+++ src/permission.js
@@ -1,10 +1,10 @@
 import router from './router'
-import store from './store'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import getPageTitle from '@/utils/get-page-title'
 import configApi from "@/api/config";
 import Cookies from "js-cookie";
+import {getToken, getUserInfo} from '@/utils/auth'
 
 import { Message } from 'element-ui'
 
@@ -15,19 +15,24 @@ const whiteList = ['/login'] // no redirect whitelist
 router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
-
   // set page title
   document.title = getPageTitle(to.meta.title)
-
-  // determine whether the user has logged in
-  const hasGetUserInfo = store.getters.userInfo
-  if (!hasGetUserInfo && whiteList.indexOf(to.path) === -1) {
-    next(`/login?redirect=${to.path}`)
-    NProgress.done()
-  }
   // 检查版本信息
   handleUpdateWeb()
-  next()
+  // determine whether the user has logged in
+  const hasGetUserInfo = getUserInfo()
+  const token = getToken()
+  if (!hasGetUserInfo || !token) {
+    // 白名单的直接放行
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      // 重新登录去
+      next(`/login?redirect=${to.path}`)
+    }
+  } else {
+    next()
+  }
 })
 
 router.afterEach(() => {
